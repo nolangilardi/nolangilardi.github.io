e5b9d74484078974e379f87b8e78c81ef89b88af
Nolan <162080300+nolangilardi@users.noreply.github.com>
2024-08-19 14:23:40 +0700

    init (#1)

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE = {
+  TITLE: 'Scratchpad',
+  DESCRIPTION: 'an attempt to organizing my chaos scratchpad',
+};
---
+++ src/content/blog/decompiling-wasm.md
@@ -0,0 +1,6 @@
+---
+title: "Decompiling WASM"
+description: "A little note to have easier time decompiling WASM before directly reading WASM very simple instruction sets."
+pubDate: "2024-08-19"
+tags: ["wasm", "reverse-engineering"]
+---
